72090fa08fd4dc1b023f610a9af40de9aaf1be77
Mason <masonandrewmann@gmail.com>
2025-09-22 02:25:09 -0400

    updated mTransitionTable

--- meap_modules/composition/mTransitionTable.h
+++ meap_modules/composition/mTransitionTable.h
@@ -1,47 +1,47 @@
 #ifndef M_TRANSITION_TABLE_H_
 #define M_TRANSITION_TABLE_H_
 
-template <int NUM_ENTRIES>
+template <int NUM_STATES>
 class mTransitionTable
 {
 public:
-    mTransitionTable(float transition_table[NUM_ENTRIES][NUM_ENTRIES])
+    mTransitionTable(float transition_table[NUM_STATES][NUM_STATES])
     {
         updateTable(transition_table);
-        current_entry = 0;
+        current_state = 0;
     }
 
-    void updateTable(float transition_table[NUM_ENTRIES][NUM_ENTRIES])
+    void updateTable(float transition_table[NUM_STATES][NUM_STATES])
     {
-        float sums[NUM_ENTRIES];
+        float sums[NUM_STATES];
 
         // copy input table and calculate sum of each row
-        for (int row = 0; row < NUM_ENTRIES; row++)
+        for (int row = 0; row < NUM_STATES; row++)
         {
             sums[row] = 0;
-            for (int col = 0; col < NUM_ENTRIES; col++)
+            for (int col = 0; col < NUM_STATES; col++)
             {
                 cumulative_table[row][col] = 0;
                 table[row][col] = transition_table[row][col];
 
                 sums[row] += table[row][col];
             }
-            // su.ms[row] = 1.f / sums[row]; // pre-inverting to save divides in the next step
+            sums[row] = 1.f / sums[row]; // pre-inverting to save divides in the next step
         }
 
         // normalizing probablity density function
-        for (int row = 0; row < NUM_ENTRIES; row++)
+        for (int row = 0; row < NUM_STATES; row++)
         {
-            for (int col = 0; col < NUM_ENTRIES; col++)
+            for (int col = 0; col < NUM_STATES; col++)
             {
-                table[row][col] /= sums[row];
+                table[row][col] *= sums[row];
             }
         }
 
         // calculate cumulative probability density function
-        for (int row = 0; row < NUM_ENTRIES; row++)
+        for (int row = 0; row < NUM_STATES; row++)
         {
-            for (int col = 0; col < NUM_ENTRIES; col++)
+            for (int col = 0; col < NUM_STATES; col++)
             {
                 for (int i = 0; i <= col; i++)
                 {
@@ -55,45 +55,30 @@ public:
     {
         float R = Meap<MEAP_DEFAULT_VERSION>::frand();
 
-        for (int i = 0; i < NUM_ENTRIES; i++)
+        for (int i = 0; i < NUM_STATES; i++)
         {
-            if (cumulative_table[current_entry][i] >= R)
+            if (cumulative_table[current_state][i] >= R)
             {
-                current_entry = i;
+                current_state = i;
                 break;
             }
         }
-        return current_entry;
+        return current_state;
     }
 
-    void setCurrentEntry(int entry)
+    void setState(int state)
     {
-        current_entry = entry;
+        current_state = state;
     }
 
-    int getCurrentEntry()
+    int getState()
     {
-        return current_entry;
+        return current_state;
     }
 
-    int print()
-    {
-        Serial.println("table");
-        for (int row = 0; row < NUM_ENTRIES; row++)
-        {
-            for (int col = 0; col < NUM_ENTRIES; col++)
-            {
-                Serial.print(cumulative_table[row][col]);
-                Serial.print(" ");
-            }
-            Serial.println(" ");
-        }
-        Serial.println("done");
-    }
-
-    int current_entry;
-    float table[NUM_ENTRIES][NUM_ENTRIES];
-    float cumulative_table[NUM_ENTRIES][NUM_ENTRIES];
+    int current_state;
+    float table[NUM_STATES][NUM_STATES];
+    float cumulative_table[NUM_STATES][NUM_STATES];
 };
 
 #endif // M_TRANSITION_TABLE_H_
